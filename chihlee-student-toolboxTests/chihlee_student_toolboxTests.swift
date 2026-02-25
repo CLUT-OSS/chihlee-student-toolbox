55504f40ac09deb9620182874221cda67b1375e5
HaeImCH <ch@thisisch.net>
2026-02-25 19:35:01 +0800

    Initial Commit

---
+++ chihlee-student-toolboxTests/chihlee_student_toolboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  chihlee_student_toolboxTests.swift
+//  chihlee-student-toolboxTests
+//
+//  Created by CH ouo on 2026/2/25.
+//
+
+import Testing
+@testable import chihlee_student_toolbox
+
+struct chihlee_student_toolboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
